30372d44be0434bbda85ddf66a3e755f8f61e24b
Mr.sobhani <102426918+Athirst1998@users.noreply.github.com>
2023-03-30 10:42:29 +0330

    Update get_ip.py

--- get_ip.py
+++ get_ip.py
@@ -1,12 +1,9 @@
 import requests
-import bs4
 from bs4 import BeautifulSoup
 import re
 
-# data = requests.get("https://www.shodan.io/search?query=cloudflare")
-# print(data.text)
-with open('index.html') as f:
-    soup = BeautifulSoup(f.read(), features="html.parser")
+data = requests.get("https://www.shodan.io/search?query=cloudflare")
+soup = BeautifulSoup(data.text, features="html.parser")
 errors = soup.find_all("div", {"class": "alert alert-error"})
 if errors:
     for error in errors:
@@ -19,23 +16,23 @@ page_number = 0
 
 for result in results:
 
-    check_cdn: bs4.element.Tag = result.find("a", {"class": "tag"})
+    check_cdn = result.find("a", {"class": "tag"})
     if check_cdn.text == 'cdn':
         print('=' * 30)
-        
+
         title = (result.find("a", {"class": "title text-dark"}))
         print(f"title: {title.text.strip()}")
-        
+
         ssl = result.find("strong")
         ssl_text = re.findall(r'>(.+)</', str(ssl))
         ssl_text = "none" if not ssl_text else ssl_text[0]
         print(f"ssl: {ssl_text.strip()}")
-        
+
         location = result.find("img", {"class": "flag"})
         location_text = re.findall(r'title="(.+)"\s', str(location))
         location_text = None if not location_text else location_text[0]
         print(f"location: {location_text.strip()}")
-        
+
         host_names = result.find_all("li", {"class": "hostnames text-secondary"})
         print('host names:')
         for name in host_names:
